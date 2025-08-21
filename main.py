401041d0a1b64e5d9379467c12602acf27e7b236
Miki004 <162804895+Miki004@users.noreply.github.com>
2025-08-21 16:44:24 +0200

    refactor: riparato il pre processing

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from preprocessing.preprocessing import preprocess_data
-
+from supervised.train import supervised_train
 if __name__ == "__main__":
 
     df_path= "student_data.csv"
     df = pd.read_csv(df_path)
-    processed_df = preprocess_data(df)
+    X_train_scaled, X_test_scaled, y_train, y_test = preprocess_data(df)
+    supervised_train(X_train_scaled, X_test_scaled, y_train, y_test)
